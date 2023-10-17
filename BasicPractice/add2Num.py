5867fe47d0b861d3194970b46e7253c9390c4e89
Noorul Huda Khan <92791314+THENHKHAN@users.noreply.github.com>
2023-10-17 12:46:09 +0530

    Update add2Num.py

--- BasicPractice/add2Num.py
+++ BasicPractice/add2Num.py
@@ -11,4 +11,12 @@ num2 = int(input("Please enter your 2nd number : "))
 sum = sumOfToNumber(num1 , num2 )
 print(f"Sum of {num1} and  {num2} is :  {sum}" )
 
+'''
+## OUTPUT
+Please enter your 1st number : 2
+Please enter your 1st number : 4
+Sum of 2 and  4 is :  6
+
+'''
+
 
