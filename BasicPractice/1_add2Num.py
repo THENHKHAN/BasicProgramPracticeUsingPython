9c8e3c64cf3b997e0cefa277936f554232ce7f51
noorulPro <knhkhan101099@gmail.com>
2023-10-18 10:46:50 +0530

    add2Num renamed and primeNumGen within interval started

---
+++ BasicPractice/1_add2Num.py
@@ -0,0 +1,22 @@
+
+#  Add two number of user choice 
+
+def sumOfToNumber(n1 , n2 ):
+    return n1+n2
+
+
+num1 = int(input("Please enter your 1st number : "))
+num2 = int(input("Please enter your 2nd number : "))
+
+sum = sumOfToNumber(num1 , num2 )
+print(f"Sum of {num1} and  {num2} is :  {sum}" )
+
+'''
+## OUTPUT
+Please enter your 1st number : 2
+Please enter your 1st number : 4
+Sum of 2 and  4 is :  6
+
+'''
+
+
